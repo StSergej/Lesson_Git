14e5b207d7feb61627f690bb42f6cbd44ef7937f
StSergej <st7cub@gmail.com>
2022-11-18 20:54:01 +0200

    edit

--- idea/src/Main.java
+++ idea/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
+        int a= 4, b = 5;
+        int c = a + b;
+        System.out.println(c);
+
         System.out.println("Hello World!");
     }
 }
