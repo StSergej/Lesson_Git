98e5f54a07f33d33f091727f4ad9fe65500a1dc9
StSergej <st7cub@gmail.com>
2022-11-18 21:00:42 +0200

    add edit

--- idea/src/Main.java
+++ idea/src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Hello, Git!");
+        System.out.println("Hello, JAVA!");
+
         int a= 4, b = 5;
         int c = a + b;
-        System.out.println(c);
-
-        System.out.println("Hello World!");
+        System.out.println("c = " + c);
     }
 }
